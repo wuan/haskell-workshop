978dd0541177a8435903ae26b90849ce92440cfe
Andreas Wuerl <andi@tryb.de>
2022-11-10 18:04:48 +0100

    first day

---
+++ app/Main.hs
@@ -0,0 +1,22 @@
+module Main (main) where
+
+main = do
+  contents <- readFile "input-1"
+  print $ whichFloor contents
+  print $ part2 $ lander contents
+
+--santa [] = 0
+--santa ('(':xs) = 1 + santa xs
+--santa (')':xs) = -1 + santa xs
+
+santa '(' = 1
+santa ')' = -1
+santa _ = 0
+
+whichFloor xs = sum (map santa xs)
+
+isPositive x = x >= 0
+
+part2 x = length $ takeWhile isPositive x
+
+lander x = scanl (+) 0 (map santa x)
