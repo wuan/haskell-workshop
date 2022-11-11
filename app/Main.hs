efb2fe15cd7accdebf6c00e90185e8f427ec0636
Andreas Wuerl <andi@tryb.de>
2022-11-11 08:09:09 +0100

    restructured days

--- app/Main.hs
+++ app/Main.hs
@@ -1,51 +1,8 @@
-module Main (main1, main) where
+module Main where
 
-import Data.List (sort)
+import Day1 (day1)
+import Day2 (day2)
 
-main1 = do
-  contents <- readFile "input-1"
-  print $ whichFloor contents
-  print $ part2 $ lander contents
-
---santa [] = 0
---santa ('(':xs) = 1 + santa xs
---santa (')':xs) = -1 + santa xs
-
-santa '(' = 1
-santa ')' = -1
-santa _ = 0
-
-whichFloor xs = sum (map santa xs)
-
-isPositive x = x >= 0
-
-part2 x = length $ takeWhile isPositive x
-
-lander x = scanl (+) 0 (map santa x)
-
--- day 2 starts here
-
-main = do
-  contents <- readFile "input-2"
-  let dimensions = map splitNumbers $ lines contents
-  print $ sum $ map requiredArea dimensions
-  print $ sum $ map requiredRibbon dimensions
-
-splitNumbers :: String -> [Int]
-splitNumbers x =
-  let l = takeWhile (not . (flip elem "x")) x
-      remainder1 = drop 1 $ dropWhile (not . (flip elem "x")) x
-      w = takeWhile (not . (flip elem "x")) remainder1
-      h = drop 1 $ dropWhile (not . (flip elem "x")) remainder1
-   in map read [l, w, h]
-
-requiredArea :: [Int] -> Int
-requiredArea [l,w,h] =
-  let areas = [l*w, w*h, h*l]
-      smallestArea = minimum areas
-  in 2 * sum areas + smallestArea
-  
-requiredRibbon :: [Int] -> Int
-requiredRibbon dimensions = 
-  let twoSmallestDimensions = take 2 $ sort dimensions
-  in 2 * sum twoSmallestDimensions + product dimensions
+main :: IO ()
+main = 
+  print "please have a look at the single day modules:"
---
+++ src/Day2.hs
@@ -0,0 +1,30 @@
+module Day2 (day2) where
+
+import Data.List (sort)
+
+day2 :: IO ()
+day2 = do
+  contents <- readFile "input-2"
+  let dimensions = map splitNumbers $ lines contents
+  print $ sum $ map requiredArea dimensions
+  print $ sum $ map requiredRibbon dimensions
+
+splitNumbers :: String -> [Int]
+splitNumbers x =
+  let l = takeWhile (not . (flip elem "x")) x
+      remainder1 = drop 1 $ dropWhile (not . (flip elem "x")) x
+      w = takeWhile (not . (flip elem "x")) remainder1
+      h = drop 1 $ dropWhile (not . (flip elem "x")) remainder1
+   in map read [l, w, h]
+
+requiredArea :: [Int] -> Int
+requiredArea [l,w,h] =
+  let areas = [l*w, w*h, h*l]
+      smallestArea = minimum areas
+  in 2 * sum areas + smallestArea
+
+requiredRibbon :: [Int] -> Int
+requiredRibbon dimensions =
+  let twoSmallestDimensions = take 2 $ sort dimensions
+  in 2 * sum twoSmallestDimensions + product dimensions
+
